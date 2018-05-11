5d059a4fce6754712d3276e7ae2605cfdfcbcd4f
ryhanlon <ryhanlon@gmail.com>
2018-05-10 20:10:45 -0700

    Built hamburger that is animated showing X when open.  Connected to the drawer.  Now drawer closes and the bars return to original position when X is clicked and when screen is clicked.

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -1,15 +1,22 @@
 "use strict";
 
+// clicking action to x, for the hamburger (from w3schools, I refactored to ECMAScript6)
+const menuClick = (event) => event.classList.toggle("change");
 
+// hamburger interaction
 const menu = document.querySelector('#menu');
 const main = document.querySelector('main');
 const drawer = document.querySelector('.nav');
+const hambugerIcon = document.querySelector('.menu-container')
 
 menu.addEventListener('click', function(e) {
 	drawer.classList.toggle('open');
 	e.stopPropagation();
 });
 
+
 main.addEventListener('click', function() {
 	drawer.classList.remove('open');
+	hambugerIcon.classList.remove('change');
 });
+
