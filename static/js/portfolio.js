5c8bbea8e965a997d2cd87da1668ffbf0be20826
ryhanlon <ryhanlon@gmail.com>
2018-05-10 23:31:44 -0700

    Styled contents of menu drawer, styled footer.  Further refined responsiveness: blog, projects, drawer/menu.

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// clicking action to x, for the hamburger (from w3schools, I refactored to ECMAScript6)
+// clicking action to event, for the hamburger (From w3schools, I refactored to ECMAScript6)
 const menuClick = (event) => event.classList.toggle("change");
 
 // hamburger interaction
 const menu = document.querySelector('#menu');
 const main = document.querySelector('main');
 const drawer = document.querySelector('.nav');
-const hambugerIcon = document.querySelector('.menu-container')
+const hamburgerIcon = document.querySelector('.menu-container');
 
 menu.addEventListener('click', function(e) {
 	drawer.classList.toggle('open');
 	e.stopPropagation();
 });
 
-
+// Closes the drawer and returns the menu icon back to ready
 main.addEventListener('click', function() {
 	drawer.classList.remove('open');
-	hambugerIcon.classList.remove('change');
+	hamburgerIcon.classList.remove('change');
 });
 
